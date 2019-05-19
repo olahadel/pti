5bd6752b546080026790f30a3af5efa542915987
olahadel <36456978+olahadel@users.noreply.github.com>
2019-05-20 00:18:00 +0200

    Add files via upload

---
+++ RSA/src/Main.java
@@ -0,0 +1,84 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RSA rsa = new RSA();
+
+        System.out.println("PRÍMEK GENERÁLÁSA:\n");
+
+        //ket prim szam generalasa
+        BigInteger p = rsa.generatePrime();
+        System.out.println(p + ": " + p.isProbablePrime(1 ) + "\n");
+        BigInteger q = rsa.generatePrime();
+
+        while ( p.equals(q) ) {
+            q = rsa.generatePrime();
+            System.out.println("p = q");
+        }
+        System.out.println(q + ": " + q.isProbablePrime(1) + "\n");
+
+        //n es phi(n) kiszamitasa
+        BigInteger n = p.multiply(q);
+        BigInteger phi_n = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+        System.out.println("n: " + n);
+        System.out.println("phi(n): " + phi_n + "\n");
+
+        //phi(n)-hez relativ kicsi e szam
+        BigInteger e = BigInteger.valueOf(2);
+        System.out.println("(" + phi_n + ", " + e + "): " + rsa.Euclid_algorithm(phi_n, e)[0] + "\t" + phi_n.gcd(e));
+        while ( rsa.Euclid_algorithm(phi_n, e)[0].compareTo(BigInteger.ONE) != 0 && e.compareTo(p) != 0 && e.compareTo(q) != 0) {
+            if (phi_n.mod(BigInteger.TWO) == BigInteger.ZERO) { //ha phi_n páros
+                e = e.add(BigInteger.ONE);
+            }
+            else {
+                e = e.add(BigInteger.TWO);
+            }
+
+            System.out.println("(" + phi_n + ", " + e + "): " + rsa.Euclid_algorithm(phi_n, e)[0] + "\t" + phi_n.gcd(e));
+        }
+
+        if (e == p || e == q) {
+            System.out.println("Nincs közös osztó!");
+        }
+
+        System.out.println("\ne: " + e);
+
+        //e * d = 1 (mod phi_n)
+        BigInteger d = rsa.Euclid_algorithm(phi_n, e)[1];
+        System.out.println("d: " + d);
+        //BigInteger PublicKey = rsa.Euclid_algorithm(e, n)[0];
+        //BigInteger SecretKey =  rsa.Euclid_algorithm(d, n)[0];
+        //System.out.println("\nPK: " + PublicKey + "\t" + e.gcd(n));
+        //System.out.println("SK: " + SecretKey + "\t" + d.gcd(n));
+
+        //titkosithato uzenetek halmaza: {0, ... , n-1 )
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("\nTitkosítandó Üzenet: ");
+
+        String in = reader.next();
+
+        BigInteger msg = BigInteger.valueOf(Integer.parseInt(in));
+
+        //encryption
+        BigInteger encrypted = rsa.QuickPow(msg, e, n);
+
+        //decryption
+        BigInteger decrypted = rsa.QuickPow(encrypted, d, n);
+
+        System.out.println("Titkosított üzenet: " + encrypted);
+        System.out.println("Visszafejtett üzenet: " + decrypted);
+
+        /*
+        //test euclid_algorithm
+        System.out.println("(160, 19): " + rsa.Euclid_algorithm(BigInteger.valueOf(534), BigInteger.valueOf(141))[0]);
+        System.out.println("d: " + rsa.Euclid_algorithm(BigInteger.valueOf(534), BigInteger.valueOf(141))[1]);
+
+        //test quickpow
+        System.out.println(rsa.QuickPow(BigInteger.valueOf(9), BigInteger.valueOf(67), BigInteger.valueOf(537)));
+        System.out.println(BigInteger.valueOf(9).modPow(BigInteger.valueOf(67), BigInteger.valueOf(537)));
+         */
+    }
+}
---
+++ RSA/src/RSA.java
@@ -0,0 +1,149 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import javax.script.Bindings;
+import java.math.BigInteger;
+import java.security.KeyPair;
+import java.util.*;
+
+public class RSA {
+    public BigInteger generatePrime() {
+        Random rand = new Random();
+        BigInteger p = new BigInteger(10, rand);
+
+        if (p.mod(BigInteger.TWO) == BigInteger.ZERO) {     //ha páros, akkor +1
+            p.add(BigInteger.ONE);
+        }
+
+        int i = 0;
+        while ( !MillerRabinTest(p, 100) && i < 1000) {      //amig nem lesz prim 2-vel noveljuk
+            p = p.add(BigInteger.TWO);
+            i++;
+            if (i == 1000) {
+                p = new BigInteger(10, rand);
+                i = 0;
+            }
+        }
+
+        System.out.println(i + " teszt volt szükséges összesen");
+        return p;
+    }
+
+    public static boolean MillerRabinTest(@NotNull BigInteger n, int numberOfTests) {
+        if (n.compareTo(BigInteger.ONE) <= 0) { //ha n <= 1
+            return false;       //nemprím
+        }
+        else if (n.compareTo(BigInteger.valueOf(3)) <= 0) {      // 1 < n <= 3
+            return true;                 //prím
+        }
+        else if (n.mod(BigInteger.TWO).equals(BigInteger.ZERO)) {       //ha n paros -> nemprím
+            return false;
+        } else {
+            for (int j = 0; j < numberOfTests; j++) {
+                BigInteger a;
+                do {
+                    a = new BigInteger(n.bitLength() - 1, new Random());    //bitLength(): 2^hanyadikon ábrázolható a szám binárisan
+                } while (a.equals(BigInteger.ZERO));    //megall, ha a!=0
+
+                // gcd(n, a) ≠ 1
+                if (!n.gcd(a).equals(BigInteger.ONE)) { //gcd=LNKO
+                    return false;                       //ha nem relativ primek -> false
+                }
+
+                // d = (n-1) / (2^S)
+                BigInteger d = n.subtract(BigInteger.ONE);  //d = n-1
+
+                // S = max{ r | 2^r osztója (n-1)-nek }
+                int S = 0;
+                while (d.mod(BigInteger.TWO).equals(BigInteger.ZERO)) { //mig d paros osztjuk 2-vel
+                    d = d.divide(BigInteger.TWO);
+                    S++;
+                }
+
+                // a^d ≡ 1 (mod n)
+                if (a.modPow(d, n).equals(BigInteger.ONE)) {    //a^d mod n
+                    return true;
+                }
+
+                // r:{0 , ... , S-1}: a^(d*2^r) ≡ -1 (mod n)
+                for (int i = 0; i <= S - 1; i++) {
+                    if (a.modPow(d.multiply(BigInteger.TWO.pow(i)), n).equals(BigInteger.ONE.negate())) {   //a^(d*2^i) mod n == -1
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+    }
+
+    public static BigInteger[] Euclid_algorithm (@NotNull BigInteger a, BigInteger b) {
+        BigInteger dividend = BigInteger.ONE;
+        BigInteger divisor = BigInteger.ONE;
+
+        if (a.compareTo(b) == 1) {     // a > b
+            dividend = a;    //osztando
+            divisor = b;     //oszto
+        } else {        // a < b
+            dividend = b;    //osztando
+            divisor = a;     //oszto
+        }
+
+        BigInteger quotient = dividend.divide(divisor);      //hanyados
+        BigInteger remainder = dividend.mod(divisor);     //maradek
+        BigInteger x_dividend = BigInteger.ONE;
+        BigInteger y_dividend = BigInteger.ZERO;
+        BigInteger x_divisor = BigInteger.ZERO;
+        BigInteger y_divisor = BigInteger.ONE;
+        BigInteger x_newcol = x_divisor.multiply(quotient).add(x_dividend);
+        BigInteger y_newcol = y_divisor.multiply(quotient).add(y_dividend);
+
+        while (remainder != BigInteger.ZERO) {
+            dividend = divisor;    //osztando
+            divisor = remainder;     //oszto
+            quotient = dividend.divide(divisor);     //hanyados
+            remainder = dividend.mod(divisor);     //maradek
+            x_dividend = x_divisor;
+            y_dividend = y_divisor;
+            x_divisor = x_newcol;
+            y_divisor = y_newcol;
+            x_newcol = x_divisor.multiply(quotient).add(x_dividend);
+            y_newcol = y_divisor.multiply(quotient).add(y_dividend);
+        }
+
+        //x = x_divisor, y = y_divisor
+        BigInteger[] result = {divisor, y_divisor};
+        return result;
+    }
+
+    public static BigInteger QuickPow(@NotNull BigInteger base, @NotNull BigInteger pow, BigInteger modulo) {
+        BigInteger remainder_class = pow;
+        Vector<Integer> mods = new Vector<>();
+        for (int i = pow.bitLength()-1; i >= 0; i--) {
+            if (BigInteger.TWO.pow(i).compareTo(remainder_class) <= 0) {     //ha 2^i < remainder
+                mods.add(i);
+                remainder_class = remainder_class.subtract(BigInteger.TWO.pow(i));
+            }
+        }
+
+        BigInteger modsmultiply = BigInteger.ONE;
+        if (mods.lastElement() == 0) {
+            mods.remove(mods.size() - 1);
+            modsmultiply = modsmultiply.multiply(base.modPow(BigInteger.TWO.pow(0), modulo));
+        }
+        remainder_class = base.modPow(BigInteger.TWO.pow(0), modulo);
+
+        for (int i=1; i < pow.bitLength(); i++) {
+            remainder_class = remainder_class.modPow(BigInteger.TWO, modulo);
+
+            if (mods.lastElement() == i) {
+                mods.remove(mods.size() - 1);
+                modsmultiply = modsmultiply.multiply(remainder_class);
+            }
+        }
+
+        return modsmultiply.mod(modulo);
+    }
+}
+
+//kiterjesztett euklideszi, kinai, csak saját fv-ek, lecsekkolni jók e az algoritmusok
