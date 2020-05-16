b37cd7f653fa8eee43732afdc844733a850dfc0e
olahadel <36456978+olahadel@users.noreply.github.com>
2020-05-16 14:10:41 +0200

    Add files via upload

--- SeekTheWord/src/main/java/controller/ResultController.java
+++ SeekTheWord/src/main/java/controller/ResultController.java
@@ -28,13 +28,27 @@ public class ResultController {
     private Label scoreLabel;
 
     @FXML
-    private Label allPossibleWordList;
+    private Label allPossibleWordList1;
 
     @FXML
     private Label allPossibleWordList2;
 
     @FXML
+    private Label allPossibleWordList3;
+
+    @FXML
+    private Label allPossibleWordList4;
+
+    @FXML
+    private Label allPossibleWordList5;
+
+    @FXML
+    private Label allPossibleWordList6;
+
+    @FXML
     private Button endGameButton;
+
+    private final int MAXWORDPERLABEL = 12;
 
     @FXML
     public void initialize() {
@@ -43,22 +57,67 @@ public class ResultController {
         scorePane.getStylesheets().add(style);
         endGameButton.getStylesheets().add(style);
         endGameButton.getStyleClass().add("shiny-orange");
-        allPossibleWordList.getStylesheets().add(style);
+        allPossibleWordList1.getStylesheets().add(style);
         allPossibleWordList2.getStylesheets().add(style);
+        allPossibleWordList3.getStylesheets().add(style);
+        allPossibleWordList4.getStylesheets().add(style);
+        allPossibleWordList5.getStylesheets().add(style);
+        allPossibleWordList6.getStylesheets().add(style);
 
         scoreLabel.setText(String.valueOf(GameController.score));
 
         logger.log(Level.INFO, "wordsetlist size: " + GameController.wordSetList.size());
 
-        StringBuilder allWords = new StringBuilder();
+        StringBuilder allWords1 = new StringBuilder();
+        StringBuilder allWords2 = new StringBuilder();
+        StringBuilder allWords3 = new StringBuilder();
+        StringBuilder allWords4 = new StringBuilder();
+        StringBuilder allWords5 = new StringBuilder();
+        StringBuilder allWords6 = new StringBuilder();
 
         int i=0;
         for (String element : GameController.wordSetList) {
-            allWords.append(element.toUpperCase() + "\n");
+            if (GameController.wordSetList.size()/MAXWORDPERLABEL == 5 || GameController.wordSetList.size()/MAXWORDPERLABEL == 4) {
+                if (i/MAXWORDPERLABEL < 1) {
+                    allWords1.append(element.toUpperCase() + "\n");
+                } else if (i/MAXWORDPERLABEL < 2) {
+                    allWords2.append(element.toUpperCase() + "\n");
+                } else if (i/MAXWORDPERLABEL < 3) {
+                    allWords3.append(element.toUpperCase() + "\n");
+                } else if (i/MAXWORDPERLABEL < 4) {
+                    allWords4.append(element.toUpperCase() + "\n");
+                } else if (i/MAXWORDPERLABEL < 5) {
+                    allWords5.append(element.toUpperCase() + "\n");
+                } else {
+                    allWords6.append(element.toUpperCase() + "\n");
+                }
+            } else if (GameController.wordSetList.size()/MAXWORDPERLABEL == 3 || GameController.wordSetList.size()/MAXWORDPERLABEL == 2) {
+                if (i/MAXWORDPERLABEL < 1) {
+                    allWords2.append(element.toUpperCase() + "\n");
+                } else if (i/MAXWORDPERLABEL < 2) {
+                    allWords3.append(element.toUpperCase() + "\n");
+                } else if (i/MAXWORDPERLABEL < 3) {
+                    allWords4.append(element.toUpperCase() + "\n");
+                } else {
+                    allWords5.append(element.toUpperCase() + "\n");
+                }
+            } else {
+                if (i/MAXWORDPERLABEL < 1) {
+                    allWords3.append(element.toUpperCase() + "\n");
+                } else {
+                    allWords4.append(element.toUpperCase() + "\n");
+                }
+            }
+
             i++;
         }
 
-        allPossibleWordList.setText(allWords.toString());
+        allPossibleWordList1.setText(allWords1.toString());
+        allPossibleWordList2.setText(allWords2.toString());
+        allPossibleWordList3.setText(allWords3.toString());
+        allPossibleWordList4.setText(allWords4.toString());
+        allPossibleWordList5.setText(allWords5.toString());
+        allPossibleWordList6.setText(allWords6.toString());
     }
 
     public void closeGame(ActionEvent actionEvent) {
